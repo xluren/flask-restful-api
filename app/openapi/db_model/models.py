a9f89c682f7b95c208afc75ab8ffe3456f7ab474
xluren <xluren@126.com>
2014-11-16 21:50:50 +0800

    add reqparse

--- app/openapi/db_model/models.py
+++ app/openapi/db_model/models.py
@@ -9,6 +9,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), index=True)
     password_hash = db.Column(db.String(64))
+
     def hash_password(self, password):
         self.password_hash = pwd_context.encrypt(password)
 
--- app/openapi/resource/userapi.py
+++ app/openapi/resource/userapi.py
@@ -2,35 +2,38 @@ from app import db
 from app.openapi.db_model.models import User
 from flask.ext import restful
 from flask     import request,jsonify,abort
+from flask.ext.restful import reqparse
 
 class userapi(restful.Resource):
     def post(self):
-        username = request.json.get('username')
-        password = request.json.get('password')
-        if username is None or password is None:
-            print "hello"
-            abort(400)    # missing arguments
+        parser = reqparse.RequestParser()
+        parser.add_argument('username', type=str, required=True)
+        parser.add_argument('password', type=str, required=True)
+        user_info = parser.parse_args()
+        username=user_info.username
+        password=user_info.password
+
         if User.query.filter_by(username=username).first() is not None:
-            print "world"
             abort(400)    # existing user
-        user = User(username=username)
+        else:
+            user = User(username=username)
         user.hash_password(password)
         db.session.add(user)
         db.session.commit()
+
         return (jsonify({"status":200,"msg":"add User success"}))
     def get(self,id):
         user = User.query.get(id)
         if not user:
             abort(400)
         return jsonify({'username': user.username})
+
     def put(self):
         username = request.json.get('username')
         password = request.json.get('password')
         if username is None or password is None:
-            print "hello"
             abort(400)    # missing arguments
         if User.query.filter_by(username=username).first() is  None:
-            print "world"
             abort(400)    # existing user
         user_list=db.session.query(User).filter_by(username=username).all()
         print len(user_list)
