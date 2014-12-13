865f0c3aee6c7568578a0ce84f6a3732fcb797d1
xluren <xluren@126.com>
2014-12-13 23:45:32 +0800

    add idc info adn hardware info

--- app/openapi/db_model/models.py
+++ app/openapi/db_model/models.py
@@ -34,3 +34,35 @@ class Servive(db.Model):
     def __repr__(self):
         return "<servive info %r,%r,%r>" % (self.servive_id,self.servive_name,self.servive_parent)
 
+class HardWare(db.Model):
+    __tablename__="hardware"
+    id=db.Column(db.Integer,primary_key=True)
+
+    server_type=db.Column(db.String(28))
+    
+    #cpuinfo eg intel#2.4Ghz#2#2;inter#2.4Gz#1#4
+    phycpu_count=db.Column(db.Intege)
+    logicalcpu_count=db.Column(db.Intege)
+    cpu_info=db.Column(db.String(28))
+
+    #mem basic info,unit:G
+    #eg 2G#10;4G#20
+    mem_size=db.Column(db.Intege)
+    mem_info=db.Column(db.String(28))
+
+    #disk basic info unit:G
+    #like ssd#100#10;sas@500#8 means ssb 100G 10 and sas 500G 8 
+    disk_info=db.Column(db.String(50))
+
+    #Network Interface Card NIC
+    nic=db.Column(db.String(50))
+class IDC(db.Model):
+    __tablename__="inc"
+    id=db.Column(db.Intege,primary_key=True)
+    idc_name=db.Column(db.String(10))
+    idc_carrier=db.Column(db.String(10))
+class Server(db.Model):
+    __tablename__="server"
+    #os_info eg: linux2.6.6 win8.0.0 or others
+    os_info=db.Column(db.String(50))
+    
